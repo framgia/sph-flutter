9ad63bf8c266d15dbcac770d82deb27a3554cf20
John Stephen Degillo <johnstephen.degillo@sun-asterisk.com>
2023-06-09 14:51:44 +0800

    Update: add comments to parameters, grouped input_field to input

--- frontend/lib/src/password_reset/password_reset_page.dart
+++ frontend/lib/src/password_reset/password_reset_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:frontend/src/components/auth/auth_header.dart';
-import 'package:frontend/src/components/input_field.dart';
+import 'package:frontend/src/components/input/input_field.dart';
 import 'package:get/get.dart';
 
 /*
