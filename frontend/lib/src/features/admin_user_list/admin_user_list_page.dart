7382dd896c9c0a741733141c04f302b667254990
IanUrriza10 <ianmichael.urriza@sun-asterisk.com>
2023-06-16 17:55:14 +0800

    Update: Used replaced the Temporary Header and Footer

--- frontend/lib/src/features/admin_user_list/admin_user_list_page.dart
+++ frontend/lib/src/features/admin_user_list/admin_user_list_page.dart
@@ -1,4 +1,6 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:frontend/src/components/input/search_field.dart';
 import 'package:frontend/src/components/templates/header_image.dart';
 import 'package:frontend/src/features/admin_user_list/components/user_list_card.dart';
@@ -25,24 +27,19 @@ class AdminUserListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(218, 234, 238, 1),
-      body: Column(
-        children: [
-          //TODO: Replace with selected Header Component
-          HeaderImage(
-            child: Container(
-              margin: const EdgeInsets.only(top: 70),
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              height: MediaQuery.of(context).size.height * 0.2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.35,
-                    child: Image.asset('assets/images/sph-flutter-logo.png'),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.4,
-                    child: FittedBox(
+      body: HeaderImage(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerRight,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: FittedBox(
+                    child: TextButton(
+                      onPressed: () {},
                       child: Text(
                         'REDEMPTO D. LEGASPI III',
                         style:
@@ -52,85 +49,85 @@ class AdminUserListPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-          Container(
-            color: const Color.fromRGBO(218, 234, 238, 1),
-            width: double.infinity,
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 30),
-            child: Stack(
-              children: [
-                Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: GestureDetector(
-                    onTap: () => Get.back(),
-                    child: const Icon(
-                      Icons.arrow_back,
-                      size: 29,
-                      color: Color.fromRGBO(120, 144, 156, 1),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.only(bottom: 30),
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: AlignmentDirectional.centerStart,
+                      child: GestureDetector(
+                        onTap: () => Get.back(),
+                        child: const Icon(
+                          Icons.arrow_back,
+                          size: 29,
+                          color: Color.fromRGBO(120, 144, 156, 1),
+                        ),
+                      ),
+                    ),
+                    const Align(
+                      alignment: AlignmentDirectional.center,
+                      child: Text(
+                        'List of Users',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromRGBO(120, 144, 156, 1),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Nunito',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: const SearchField(
+                  name: "name",
+                ),
+              ),
+              Container(
+                height: 10,
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  child: MediaQuery.removePadding(
+                    removeTop: true,
+                    context: context,
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        String name = userList[index];
+                        return UserListCard(name: name);
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const Divider(
+                          height: 1,
+                          color: Color.fromRGBO(109, 120, 129, 1),
+                        );
+                      },
+                      itemCount: userList.length,
                     ),
                   ),
                 ),
-                const Align(
-                  alignment: AlignmentDirectional.center,
-                  child: Text(
-                    'List of Users',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Color.fromRGBO(120, 144, 156, 1),
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Nunito',
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              )
+            ],
           ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            width: double.infinity,
-            alignment: Alignment.center,
-            child: const SearchField(
-              name: "name",
-            ),
-          ),
-          Container(
-            height: 10,
-          ),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              color: Colors.white,
-              child: MediaQuery.removePadding(
-                removeTop: true,
-                context: context,
-                child: ListView.separated(
-                  shrinkWrap: true,
-                  itemBuilder: (BuildContext context, int index) {
-                    String name = userList[index];
-                    return UserListCard(name: name);
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const Divider(
-                      height: 1,
-                      color: Color.fromRGBO(109, 120, 129, 1),
-                    );
-                  },
-                  itemCount: userList.length,
-                ),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
-
-      // TODO (ian): replace temporary widget with Navbar Widget
-      bottomNavigationBar: Container(
-        color: const Color.fromRGBO(93, 138, 153, 1),
-        height: 45,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: const Color(0xFFDAEAEE),
+        color: const Color(0xFF5D8A99),
+        items: <Widget>[
+          SvgPicture.asset('assets/svg/home.svg'),
+          SvgPicture.asset('assets/svg/profile_settings.svg'),
+        ],
+        onTap: (index) {},
       ),
     );
   }
