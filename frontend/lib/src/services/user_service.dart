9a1eadf26a5d40e76d58a233bed1445b79a040d9
John Stephen Degillo <95029803+jstephend-sun@users.noreply.github.com>
2023-07-03 16:14:26 +0800

    [SIM_FLUTTER-94] [Integrate] User should stay logged in once user goes back to the application (#30)
    * [SIM_FLUTTER-78] [Integrate] Display users dynamically
    
    * [SIM_FLUTTER-94] [Integrate] User should stay logged in once user goes back to the application

--- frontend/lib/src/services/user_service.dart
+++ frontend/lib/src/services/user_service.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:frontend/src/helper/dio.dart';
+import 'package:frontend/src/helper/storage.dart';
 import 'package:frontend/src/models/user.dart';
 
 class UserService {
+  // returns List of User(model) by hitting /users route, non-admin users only
   static Future<List<User>> getUsers() async {
     final usersResponse = await NetworkConfig().client.get(usersUrl);
 
@@ -16,5 +19,22 @@ class UserService {
     } else {
       return [];
     }
+  }
+
+  // returns bool, checks if storage has login token
+  static Future<bool> hasLoginToken() async {
+    const storage = FlutterSecureStorage();
+    final token = await storage.read(key: StorageKeys.loginToken.name);
+
+    return token != null;
+  }
+
+  // returns bool, checks if login token is still valid by hitting /user
+  static Future<bool> checkLoginToken() async {
+    final client = NetworkConfig().client;
+
+    final userResponse = await client.get(userUrl);
+
+    return userResponse.statusCode == HttpStatus.ok;
   }
 }
