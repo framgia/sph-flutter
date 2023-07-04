9e08a289991ebf1ef18f3642977ca78b00d17a8e
Egie Garciano <egie.garciano@sun-asterisk.com>
2023-07-04 14:22:13 +0800

    [SIM_FLUTTER-39] [INTEGRATE] Implement user can sign-up

--- frontend/lib/src/features/sign_up/sign_up_step_one.dart
+++ frontend/lib/src/features/sign_up/sign_up_step_one.dart
@@ -1,130 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:get/get.dart';
 
+import 'package:frontend/src/components/breadcrumb.dart';
 import 'package:frontend/src/features/sign_up/sign_up_flow.dart';
-import 'package:frontend/src/features/sign_up/components/breadcrumb.dart';
 import 'package:frontend/src/components/input/input_field.dart';
 import 'package:frontend/src/components/button.dart';
-import 'package:frontend/src/features/login/login_page.dart';
 import 'package:frontend/src/components/input/date_picker_field.dart';
-import 'package:frontend/src/components/auth/auth_header.dart';
+import 'package:frontend/src/controllers/sign_up_controller.dart';
 
 /*
   The page where user can fill the first page of sign up.
+
+  @param, userFormData, the current sign up flow data.
 */
 class SignUpStepOne extends StatelessWidget {
-  const SignUpStepOne({super.key});
+  const SignUpStepOne({super.key, required this.userFormData});
+
+  final UserFormData userFormData;
 
   @override
   Widget build(BuildContext context) {
+    final SignUpController signUpController = Get.put(SignUpController());
     final formKey = GlobalKey<FormBuilderState>();
 
-    return GestureDetector(
-      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            AuthHeader(
+    void onSubmit() {
+      context.flow<UserFormData>().update(
+            (data) => data.copyWith(
+              page: 2,
+              firstName: formKey.currentState!.fields['first_name']!.value,
+              middleName: formKey.currentState!.fields['middle_name']!.value,
+              lastName: formKey.currentState!.fields['last_name']!.value,
+              address: formKey.currentState!.fields['address']!.value,
+              birthday: formKey.currentState!.fields['birthday']!.value,
+            ),
+          );
+    }
+
+    return GetX<SignUpController>(
+      builder: (_) => Column(
+        children: [
+          const Breadcrumb(
+            text: 'Sign Up',
+            withBackIcon: false,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25, 30, 25, 0),
+            child: FormBuilder(
+              key: formKey,
+              autovalidateMode: AutovalidateMode.disabled,
+              onChanged: () {
+                formKey.currentState!.save();
+
+                signUpController.setSignUpFirstButtonEnabled =
+                    formKey.currentState!.isValid;
+              },
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Breadcrumb(
-                    text: 'Sign Up',
+                  InputField(
+                    label: 'First Name',
+                    name: 'first_name',
+                    initialValue: userFormData.firstName,
+                    validator: FormBuilderValidators.required(),
+                  ),
+                  const SizedBox(height: 26),
+                  InputField(
+                    label: 'Middle Name',
+                    name: 'middle_name',
+                    initialValue: userFormData.middleName,
+                  ),
+                  const SizedBox(height: 26),
+                  InputField(
+                    label: 'Last Name',
+                    name: 'last_name',
+                    validator: FormBuilderValidators.required(),
+                    initialValue: userFormData.lastName,
+                  ),
+                  const SizedBox(height: 26),
+                  InputField(
+                    label: 'Address',
+                    name: 'address',
+                    validator: FormBuilderValidators.required(),
+                    initialValue: userFormData.address,
+                  ),
+                  const SizedBox(height: 26),
+                  DatePickerField(
+                    labelText: 'Birthday',
+                    name: 'birthday',
+                    validator: FormBuilderValidators.required(),
+                    initialValue: userFormData.birthday,
+                    lastDate: DateTime.now(),
                   ),
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(25, 30, 25, 150),
-                    child: FormBuilder(
-                      key: formKey,
-                      autovalidateMode: AutovalidateMode.always,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const InputField(
-                            label: 'First Name',
-                            name: 'first_name',
-                            inputType: TextInputType.text,
+                    padding: const EdgeInsets.only(top: 50, bottom: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Button(
+                          text: 'Next',
+                          enabled: signUpController.signUpFirstButtonEnabled,
+                          onPressed: onSubmit,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 99,
+                            vertical: 16,
                           ),
-                          const SizedBox(height: 26),
-                          const InputField(
-                            label: 'Middle Name',
-                            name: 'middle_name',
-                            inputType: TextInputType.text,
-                          ),
-                          const SizedBox(height: 26),
-                          const InputField(
-                            label: 'Last Name',
-                            name: 'last_name',
-                            inputType: TextInputType.text,
-                          ),
-                          const SizedBox(height: 26),
-                          const InputField(
-                            label: 'Address',
-                            name: 'address',
-                            inputType: TextInputType.text,
-                          ),
-                          const SizedBox(height: 26),
-                          const DatePickerField(
-                            name: 'birthday',
-                            labelText: 'Birthday',
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 50, bottom: 25),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Button(
-                                  text: 'Next',
-                                  onPressed: () {
-                                    context.flow<UserFormData>().update(
-                                          (data) => data.copyWith(page: 2),
-                                        );
-                                  },
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 99,
-                                    vertical: 16,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Already have an account? ',
-                                style: Theme.of(context).textTheme.labelSmall,
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  Get.to(() => const LoginPage());
-                                },
-                                child: Text(
-                                  'Log in',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelSmall!
-                                      .copyWith(
-                                        color: const Color.fromARGB(
-                                          255,
-                                          0,
-                                          163,
-                                          255,
-                                        ),
-                                      ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
