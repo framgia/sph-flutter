83555b6f9dafd6449a0ce8da3818b9329999d052
John Stephen Degillo <95029803+jstephend-sun@users.noreply.github.com>
2023-07-19 14:57:47 +0800

    [SIM_FLUTTER-123] [BUG] [BE] TransactionController index not working on /transactions route (#76)

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\Transaction;
 use App\Observers\TransactionObserver;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
