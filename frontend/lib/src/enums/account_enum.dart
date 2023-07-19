5a5c6972b0209bf48738e7a823b8abe31b0f711f
Egie Garciano <egie.garciano@sun-asterisk.com>
2023-07-19 17:16:37 +0800

    update: removed comma

--- frontend/lib/src/enums/account_enum.dart
+++ frontend/lib/src/enums/account_enum.dart
@@ -11,12 +11,12 @@ enum AccountType {
   const AccountType(this.value);
 
   // Parse the custom value and return the matching enum type.
-  // Ex. AccountType.fromValue('Savings') will return AccountType.SAVINGS,
+  // Ex. AccountType.fromValue('Savings') will return AccountType.SAVINGS
   static AccountType fromValue(String value) =>
       AccountType.values.singleWhere((i) => value == i.value);
 
   // Parse the json data from BE and return the matching enum type.
-  // Ex. AccountType.fromJson('SAVINGS') will return AccountType.SAVINGS,
+  // Ex. AccountType.fromJson('SAVINGS') will return AccountType.SAVINGS
   static AccountType fromJson(String jsonValue) =>
       AccountType.values.singleWhere((i) => jsonValue == i.name);
 }
--- frontend/lib/src/enums/transaction_enum.dart
+++ frontend/lib/src/enums/transaction_enum.dart
@@ -10,12 +10,12 @@ enum TransactionType {
   const TransactionType(this.value);
 
   // Parse the custom value and return the matching enum type.
-  // Ex. TransactionType.fromValue('Credit') will return TransactionType.CREDIT,
+  // Ex. TransactionType.fromValue('Credit') will return TransactionType.CREDIT
   static TransactionType fromValue(String value) =>
       TransactionType.values.singleWhere((i) => value == i.value);
 
   // Parse the json data from BE and return the matching enum type.
-  // Ex. TransactionType.fromJson('CREDIT') will return TransactionType.CREDIT,
+  // Ex. TransactionType.fromJson('CREDIT') will return TransactionType.CREDIT
   static TransactionType fromJson(String jsonValue) =>
       TransactionType.values.singleWhere((i) => jsonValue == i.name);
 }
@@ -42,12 +42,12 @@ enum TransactionCategory {
   ];
 
   // Parse the custom value and return the matching enum type.
-  // Ex. TransactionCategory.fromValue('Savings') will return TransactionCategory.SAVINGS,
+  // Ex. TransactionCategory.fromValue('Savings') will return TransactionCategory.SAVINGS
   static TransactionCategory fromValue(String value) =>
       TransactionCategory.values.singleWhere((i) => value == i.value);
 
   // Parse the json data from BE and return the matching enum type.
-  // Ex. TransactionCategory.fromJson('SAVINGS') will return TransactionCategory.SAVINGS,
+  // Ex. TransactionCategory.fromJson('SAVINGS') will return TransactionCategory.SAVINGS
   static TransactionCategory fromJson(String jsonValue) =>
       TransactionCategory.values.singleWhere((i) => jsonValue == i.name);
 }
